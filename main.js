a71cac79251ffaee1e16c8bfe9e7ef513a400d5f
AndrewHemry <Andrewhemry@gmail.com>
2022-10-09 20:25:27 -0500

    Updating Mastermind with final versioning

--- main.js
+++ main.js
@@ -10,6 +10,9 @@ const rl = readline.createInterface({
 let board = [];
 let solution = '';
 let letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+let correctLetters = 0;
+let correctLetterLocations = 0;
+let numberOfTurns = 10;
 
 const printBoard = () =>  {
   for (let i = 0; i < board.length; i++) {
@@ -28,13 +31,55 @@ const getRandomInt = (min, max) => {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
-const generateHint = () =>  {
-  // your code here
+const generateHint = (guess) =>  {
+  let solutionArray = solution.split("");
+  let guessArray = guess.split("");
+  console.log(solutionArray);
+  console.log(guessArray);
+
+  // This for loop is to increase the correct letters guessed counter and remove the first matching array value
+  for (let i = 0; i < solutionArray.length; i++) {
+    if (solutionArray[i] === guessArray[i]) {
+      correctLetterLocations++;
+      solutionArray[i] = null
+    }
+  }
+
+  // This for loop is to compare the Guess & Solution Array, then remove any duplicate letters that may match the original indexed value
+  for (let i = 0; i < solutionArray.length; i++) {
+    let indexLetterGuessed = guessArray[i]
+    let targetIndex = solutionArray.indexOf(indexLetterGuessed)
+    if (targetIndex !== -1)  //Can this be !==?
+    { 
+      correctLetters++;
+      solutionArray[targetIndex] = null
+    }
+  }
+  
+
+  const hint = `Your guess was ${guess}. The number of correct letters NOT in the proper location is currently ${correctLetters}, with ${correctLetterLocations} of the correct letters in the proper location. You have ${numberOfTurns} turns remaining! Choose wisly.`;
+  board.push(hint);
+
+  // This is to reset the number of both correct letter variables so they don't consistently add
+  correctLetterLocations = 0;
+  correctLetters = 0;
+
 }
 
 const mastermind = (guess) => {
-  solution = 'abcd'; // Comment this out to generate a random solution
-  // your code here
+  // Comment this out to generate a random solution
+  solution = 'abcd';
+
+  if (guess == solution) {
+    console.log("You guessed it!")
+    return "You guessed it!";
+    } else if (numberOfTurns == 0) { 
+     console.log(`You have ran out of turns! The solution was ${solution}. Reset and try again.`)
+     return `You have ran out of turns! The solution was ${solution}. Reset and try again.`
+    } else {
+    numberOfTurns--
+    generateHint(guess)
+  }
 }
 
 
